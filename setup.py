5f6f86e61dd7449df0e40922f21003fd889b4c66
Travis A. O'Brien <obrienta@iu.edu>
2023-11-03 08:42:55 -0400

    added license

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     install_requires=[
         "mpi4py",
     ],
+    license="BSD-3-Clause",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
