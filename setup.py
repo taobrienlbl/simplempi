7d85e6def2e4afe4f5e1cc427fd577a5819c7f63
Travis A. O'Brien <obrienta@iu.edu>
2023-11-03 09:29:26 -0400

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="simplempi",
-    version='0.1.1',
+    version='0.1.2',
     description="A wrapper around mpi4py that offers simple scattering of iterable objects.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
